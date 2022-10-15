bebfb023fde9181f12eb81cacb5648cd5ef05165
Fernando Alvarez <fernando.alvarez@creativecoefficient.net>
2022-10-15 18:18:25 -0300

    Commit inicial - instalación de React JS + eslint + prettier

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <span>Airbnb - Grupo 2 - Admin I</span>
+    </header>
+  </div>
+);
+
+export default App;
