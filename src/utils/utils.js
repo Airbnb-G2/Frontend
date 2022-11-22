ff1a3b63fe8688facd48b1dd207b8532a6be849c
feeerr95 <feeerr95@gmail.com>
2022-11-21 22:55:51 -0300

    get disabled dates (#18)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,3 +5,14 @@ const priceFormatter = new Intl.NumberFormat('es-ar');
 export const formatDate = (date) => moment(date).format('YYYY-MM-DD');
 
 export const formatPrice = (price) => priceFormatter.format(Number(price));
+
+export const getDatesInRange = (startDate, endDate) => {
+  const dateArray = [];
+  let currentDate = moment(startDate);
+  const formattedEndDate = moment(endDate);
+  while (currentDate <= formattedEndDate) {
+    dateArray.push(moment(currentDate).toDate());
+    currentDate = moment(currentDate).add(1, 'days');
+  }
+  return dateArray;
+};
